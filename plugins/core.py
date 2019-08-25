2fe6e15db80e37932db69435d87e9c908ede9990
Christy Roys <royschristy@gmail.com>
2019-08-25 08:27:14 +0000

    Update core.py

--- plugins/core.py
+++ plugins/core.py
@@ -31,10 +31,12 @@ def _core(c, m):
     try:
         
         r = session.get(req_url)
+        print(r.status_code)
         
         soup = BeautifulSoup(r.text, "html.parser")
         
         article = soup.find("div", id="article")
+        print(article)
         
         if(not article):
             
